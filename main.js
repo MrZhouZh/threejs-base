0fb4b8aaf699464ab53dddc44587cf1b45e58b79
MrZhouZh <17600330793@sina.cn>
2023-06-02 23:48:37 +0800

    styles: updated files

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import createScene from './scripts/scene.js'
 import drawLine from './scripts/line.js'
 import createText from './scripts/createText.js'
+import './style.css'
 
 createScene()
 drawLine()
