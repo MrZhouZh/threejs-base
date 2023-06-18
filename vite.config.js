06a87f2f1fd5ea0463f14d737fbb35483bc8bfa0
MrZhouZh <17600330793@sina.cn>
2023-06-18 16:37:31 +0800

    feat: add gzip

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
+  plugins: [viteCompression()],
   build: {
     rollupOptions: {
       output: {
